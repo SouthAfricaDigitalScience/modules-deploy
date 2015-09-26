8bcfdfb689311152269148d5b23f75882de848ce
Bruce Becker <brucellino@gmail.com>
2015-09-26 18:32:12 +0200

    how did i miss a bloody linE???

--- build.sh
+++ build.sh
@@ -37,4 +37,5 @@ fi
 tar -xvzf $SRC_DIR/$SOURCE_FILE -C $WORKSPACE
 cd $WORKSPACE/$NAME-$VERSION
 # see http://sourceforge.net/p/modules/bugs/62/
+CPPFLAGS="-DUSE_INTERP_ERRORLINE" ./configure --with-tcl=${SOFT_DIR}/tcl8/lib --with-tcl-ver=8.6 --without-tclx
 make -j 8
